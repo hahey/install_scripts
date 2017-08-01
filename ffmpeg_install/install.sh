360fd3aecaefac18063e4e48909be0f2f62de36a
hahey <heynaheyna9@gmail.com>
2017-08-01 11:09:56 +0200

    ffmpeg install

---
+++ ffmpeg_install/install.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+sudo apt-get -y install autoconf automake build-essential
+
+sudo apt-get -y install libass-dev libfreetype6-dev\
+    libsdl2-dev libtheora-dev libtool libva-dev libvpau-dev \
+    libvorbis-dev libxcbl-dev libxcb-shm0-dev libxcb-xfixes0-dev
+
+sudo apt-get -y install pkg-config texinfo wget zlib1g-dev
+
+sudo apt-get -y install yasm nasm libx264 libx265 \
+    libfdk-aac libmp3lame libopus libbpx
+
+sudo apt-get -y install ffmpeg frie0r-plugins
+
